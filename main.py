59aa24dc14a0975ef1b3e08508c265f5fa54d283
Sergii Vostrikov <sergey.a.vostrikov@gmail.com>
2017-10-28 18:56:24 +0300

    decision reply added to main behavior

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import config
+import decision
 import telebot
 
 import re
@@ -21,17 +22,20 @@ bot = telebot.TeleBot(config.token)
 
 #https://github.com/eternnoir/pyTelegramBotAPI#methods
 
-@bot.message_handler(content_types=["sticker"]) # content_types["text", "sticker", "pinned_message", "photo", "audio"]
-def sticker_handler(message):
-    bot.reply_to(message, "pack: %(pack)s\n" \
-                          "emoji: %(emoji)s" %  \
-                            dict(pack  = message.sticker.set_name, \
-                                 emoji = message.sticker.emoji     \
-                            ))
+#@bot.message_handler(content_types=["sticker"]) # content_types["text", "sticker", "pinned_message", "photo", "audio"]
+#def sticker_handler(message):
+#    bot.reply_to(message, "pack: %(pack)s\n" \
+#                          "emoji: %(emoji)s" %  \
+#                            dict(pack  = message.sticker.set_name, \
+#                                 emoji = message.sticker.emoji     \
+#                            ))
 
 @bot.message_handler(content_types=["text"], regexp=config.trigger_regex)
-def repeat_all_messages(message):
-    bot.reply_to(message, message.text)
+def repeat_addressed_messages(message):
+    question = decision.extract_question(message.text, config.trigger_regex)
+    if question:
+        answers = decision.split_question(question)
+        bot.reply_to(message, decision.decide(answers))
 
 sys.tracebacklimit = 0
 
