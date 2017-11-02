d9aac696a78444db14ddee8da38aa0fa0e9f2725
Sergii Vostrikov <sergey.a.vostrikov@gmail.com>
2017-11-02 23:38:02 +0200

    /anecdot no longer available due to safety reasons

--- anecdot.py
+++ anecdot.py
@@ -7,12 +7,14 @@ import urllib2
 from lxml import html
 
 def get_anecdot():
-    response = urllib2.urlopen('https://pda.anekdot.ru/anekdots/random/')
-    text = response.read()
-    text = text.replace("<br>", "\n")
-    tree = html.fromstring(text)
-    p = tree.xpath("//p")
-    return unicode(p[0].text)
+    return "Приходит как-то человек в ботобар и говорит /anecdot, а бот ему отвечет: this->anecdot()"
+### you'll get banned if to use following:
+#    response = urllib2.urlopen('https://pda.anekdot.ru/anekdots/random/')
+#    text = response.read()
+#    text = text.replace("<br>", "\n")
+#    tree = html.fromstring(text)
+#    p = tree.xpath("//p")
+#    return unicode(p[0].text)
 
 if __name__ == '__main__':
     print(get_anecdot())
--- main.py
+++ main.py
@@ -66,5 +66,5 @@ if __name__ == '__main__':
     while True:
         try:
             bot.polling(none_stop=True)
-        except urllib.error.HTTPError:
+        except Exception: # recover from any error, please
             time.sleep(10)
