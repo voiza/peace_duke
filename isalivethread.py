8cd977a07254432ea75e738ed8c757823cf459bd
Sergii Vostrikov <sergey.a.vostrikov@gmail.com>
2021-07-09 14:20:30 +0300

    small log killer logging adjustments

--- isalivethread.py
+++ isalivethread.py
@@ -2,6 +2,10 @@
 
 import threading
 import time
+from datetime import datetime
+
+def log(s):
+    print("{} {}".format(datetime.now().strftime("%Y/%m/%d-%H:%M:%S"), s))
 
 class IsAliveThread(threading.Thread):
     def __init__(self, telebot, sleep_time=60):
@@ -16,8 +20,9 @@ class IsAliveThread(threading.Thread):
             try:
                 killer = threading.Timer(self.timeout, self.kill_bot)
                 killer.start()
-            except Exception:
-                print("killer is killed")
+                log("killer started")
+            except Exception as e:
+                log("killer could not start: {}".format(e))
                 time.sleep(self.sleep_time)
                 continue
 
@@ -30,5 +35,5 @@ class IsAliveThread(threading.Thread):
             time.sleep(self.sleep_time)
 
     def kill_bot(self):
-        print("killing")
+        log("killing")
         self.telebot.stop_polling()
